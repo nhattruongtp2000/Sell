a395e7f5a5f7af1539c09b8b1b39277eac154c8d
Truong <nhattruongtp2000@gmail.com>
2020-11-08 10:47:52 +0700

    16

--- Sell/Startup.cs
+++ Sell/Startup.cs
@@ -98,6 +98,8 @@ namespace Sell
                 routes.MapRoute(
                     name: "default",
                     template: "{controller=Home}/{action=Index}/{id?}");
+                routes.MapRoute(name: "areaRoute",
+               template: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
             });
         }
     }
